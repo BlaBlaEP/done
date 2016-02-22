7c952cfe2dd56a681bd452d3e0d9a3c4c6498342
BlaBlaEP <stephan.selim@epfl.ch>
2016-02-22 11:29:11 +0100

    Added files via upload

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+/**
+ * @file hello_world.c
+ * @brief Prints a user-friendly, though not original, welcome message.
+ *
+ * A simple "hello world!" program, to be translated to French.
+ *
+ * @author J.-C. Chappelier
+ * @date 5 Fev 2016
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    puts("Hello world!");
+    return 0;
+}
